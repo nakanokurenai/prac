d66ca0f1975cad71c02c8793464c1c3654e83340
nakanokurenai <master@yukarium.net>
2019-10-29 00:08:22 +0900

    polish_notation: Add BINARY_OPERATIONS hash

--- fp2019/ex4_polish_notation.rb
+++ fp2019/ex4_polish_notation.rb
@@ -1,22 +1,34 @@
+# 二項演算子 のみ
+#   "+" 加算
+#   "-" 除算
+#   "*" 乗算
+#   "/" 除算
+#   "^" 冪乗
+#   "%" 余剰
+
+BINARY_OPERATIONS = {
+  "+" => -> (l, r) { l + r },
+  "-" => -> (l, r) { l - r },
+  "*" => -> (l, r) { l * r },
+  "/" => -> (l, r) { l / r },
+  "^" => -> (l, r) { l ** r },
+  "%" => -> (l, r) { l % r },
+}
+BINARY_OPERATORS = BINARY_OPERATIONS.keys
+OPERATORS = BINARY_OPERATORS
+
 def op?(c)
-  operations = ["+", "-", "*", "/", "^"]
-  operations.include?(c)
+  OPERATORS.include?(c)
 end
-def calculate(op, *args)
-  left, right = args
-  case op
-  when "+"
-    left + right
-  when "-"
-    left - right
-  when "*"
-    left * right
-  when "/"
-    left / right
-  when "^"
-    left ** right
+
+def calculate!(op, *args)
+  raise "NOT OP." unless OPERATORS.include?(op)
+  if BINARY_OPERATORS.include?(op)
+    left, right = args
+	return BINARY_OPERATIONS[op].call(left, right)
   end
 end
+
 def grouping(a)
   return [a.first.to_i, 1] unless op?(a.first)
   seek = 1
@@ -26,18 +38,19 @@ def grouping(a)
   seek += rs
   [[a.first, lv, rv], seek]
 end
-def visit(tree)
+
+def visit!(tree)
   op = tree.first
-  calculate(
+  calculate!(
     op,
-    tree[1].is_a?(Array) ? visit(tree[1]) : tree[1],
-    tree[2].is_a?(Array) ? visit(tree[2]) : tree[2],
+    tree[1].is_a?(Array) ? visit!(tree[1]) : tree[1],
+    tree[2].is_a?(Array) ? visit!(tree[2]) : tree[2],
   )
 end
 
 def calc_pn(s)
   tree, = grouping(s.split ' ')
-  visit(tree)
+  visit!(tree)
 end
 
 puts "ポーランド記法:"
